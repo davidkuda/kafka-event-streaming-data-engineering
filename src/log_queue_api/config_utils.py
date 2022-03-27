1dff6fd3f9a9e28fa5aad47d221d0bdd60ee6e6b
David Kuda <david.kuda@pm.me>
2022-03-27 18:14:22 +0200

    Remove other config option

--- src/log_queue_api/config_utils.py
+++ src/log_queue_api/config_utils.py
@@ -9,16 +9,3 @@ def get_config():
     config_parser.read_file(open("config/kafka_config.ini", "r"))
     config = dict(config_parser["default"])
     return config
-
-
-def parse_config_file(config_file: str = "config/kafka_config.ini") -> dict:
-    config = ConfigParser()
-    config.read_file(open(config_file))
-    sections = config.sections()
-    configs = {}
-    for section in sections:
-        for item in config[section].items():
-            k = item[0].upper()
-            v = item[1]
-            configs.update({k: v})
-    return configs
