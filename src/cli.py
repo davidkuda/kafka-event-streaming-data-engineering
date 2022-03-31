b365d3ec45f8cfeb9dd229e607646aab36784647
David Kuda <david.kuda@pm.me>
2022-03-31 22:05:26 +0200

    Delete cli cmds from other project

--- src/cli.py
+++ src/cli.py
@@ -3,7 +3,6 @@ import json
 import typer
 
 from log_queue_api import kafka_io
-from log_queue_api import sqlite3_io
 from log_queue_api.event_handlers import handle_org_events, handle_user_events
 
 
@@ -11,19 +10,13 @@ app = typer.Typer()
 
 
 @app.command()
-def init():
-    topics = ["user_events", "org_events"]
-    kafka_io.create_topics(topics)
-    db = sqlite3_io.Sqlite3Connection()
-    db.create_tables()
+def create_topics():
+    kafka_io.create_topics(["website_visits", "visits_per_minute"])
 
 
 @app.command()
-def teardown():
-    topics = ["user_events", "org_events"]
-    kafka_io.delete_topics(topics)
-    db = sqlite3_io.Sqlite3Connection()
-    db.remove_existing_db_file()
+def delete_topics():
+    kafka_io.delete_topics(["website_visits", "visits_per_minute"])
 
 
 @app.command()
@@ -44,19 +37,6 @@ def subscribe(topic: str):
 @app.command()
 def listen_to_user_events():
     kafka_io.subscribe("user_events", handle_user_events)
-
-
-@app.command()
-def listen_to_org_events():
-    kafka_io.subscribe("org_events", handle_org_events)
-
-
-@app.command()
-def stream_user_events():
-    with open("./data/user_events.json") as f:
-        j = json.load(f)
-    for i in j:
-        kafka_io.push("user_events", i["received_at"], json.dumps(i))
 
 
 @app.command()
